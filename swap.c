fc579fc459af4c92177fbf6e944c254e0416b524
Arun720 <84713038+Arun720@users.noreply.github.com>
2021-07-02 10:28:46 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,31 @@
+void swap_max(int a[], int L, int n)
+{
+  int swp=a[n];
+  int count=n;
+  for(int i=n ; i<L ; i++)
+  {
+    if(a[i]>swp)
+    {
+      swp=a[i];
+      count=i;
+    } 
+  }
+  a[count]=a[n];
+  a[n]=swp;
+} 
+
+
+void ssort(int arr[], int l)
+{
+  for(int p=0;p<l;p++)  
+  {
+    swap_max(arr,l,p);
+  }
+  printf("Sorted Array:\n");
+
+  for(int p=0;p<l;p++)
+  {
+      printf("%d ",arr[p]); 
+  }
+
+}
